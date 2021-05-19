cf9118b768a588060cf4d2257e976459711e7cc9
Serhii Nikitin <serg.niki@i.ua>
2021-05-19 22:28:07 +0300

    header and first section were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// $(document).ready(function() {
+//   $('.header__hamburger-open' ).click(function() {
+//     $('.header__nav').slideToggle( "slow")
+//     $('.header__hamburger-close').css({display: "block"});
+//     $('.header__hamburger-open').css({display: "none"});
+//   });
+
+//   $('.header__hamburger-close').click(function() {
+//     $('.header__nav').slideToggle( "slow")
+//     $('.header__hamburger-close').css({display: "none"});
+//     $('.header__hamburger-open').css({display: "block"});
+//   });
+// });
