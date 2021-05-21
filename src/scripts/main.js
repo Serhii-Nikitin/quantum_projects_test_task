c9146b1c1ac39e7647c1ffb73862207a2c32fc5f
Serhii Nikitin <serg.niki@i.ua>
2021-05-21 13:41:33 +0300

    secont and third sections were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,48 @@
 'use strict';
 
-// $(document).ready(function() {
-//   $('.header__hamburger-open' ).click(function() {
-//     $('.header__nav').slideToggle( "slow")
-//     $('.header__hamburger-close').css({display: "block"});
-//     $('.header__hamburger-open').css({display: "none"});
-//   });
+$(document).ready(function() {
+  $('.header__hamburger').click(function() {
+    $('.header__nav').toggleClass('header__nav--active')
+    $('.header__hamburger').toggleClass('header__hamburger--close');
+  });
 
-//   $('.header__hamburger-close').click(function() {
-//     $('.header__nav').slideToggle( "slow")
-//     $('.header__hamburger-close').css({display: "none"});
-//     $('.header__hamburger-open').css({display: "block"});
-//   });
-// });
+  $('.comments__button--right').click(function() {
+    var currentComment = $('.comments__comment-container--active');
+    var currentCommentIndex = $('.comments__comment-container--active').index();
+    var nextCommentIndex = currentCommentIndex + 1;
+    var nextComment = $('.comments__comment-container').eq(nextCommentIndex);
+    currentComment.fadeOut(0);
+    currentComment.removeClass('comments__comment-container--active');
+
+    if (currentCommentIndex == ($('.comments__comment-container:first').index())) {
+      $('.comments__button--left').prop('disabled', false);
+    };
+
+    if (nextCommentIndex == ($('.comments__comment-container:last').index())) {
+      $('.comments__button--right').prop('disabled', true);
+    };
+
+    nextComment.fadeIn(0);
+    nextComment.addClass('comments__comment-container--active');
+  });
+
+  $('.comments__button--left').click(function() {
+    var currentComment = $('.comments__comment-container--active');
+    var currentCommentIndex = $('.comments__comment-container--active').index();
+    var prevCommentIndex = currentCommentIndex - 1;
+    var prevComment = $('.comments__comment-container').eq(prevCommentIndex);
+
+    if (currentCommentIndex == ($('.comments__comment-container:last').index())) {
+      $('.comments__button--right').prop('disabled', false);
+    };
+
+    if (prevCommentIndex == ($('.comments__comment-container:first').index())) {
+      $('.comments__button--left').prop('disabled', true);
+    };
+
+    currentComment.fadeOut(0);
+    currentComment.removeClass('comments__comment-container--active');
+    prevComment.fadeIn(0);
+    prevComment.addClass('comments__comment-container--active');
+  })
+});
