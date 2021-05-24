d05ae9019664fe8491bed54c2000755163b444d9
Serhii Nikitin <serg.niki@i.ua>
2021-05-24 21:10:53 +0300

    quantum test task v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -59,4 +59,83 @@ $(document).ready(function() {
     $(this).toggleClass('questions__question-toggler--close');
     $(this).parent().next().slideToggle(100);
   });
+
+  const regexp = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
+  const mail = $('.contact-us__email');
+  const name = $('.contact-us__name');
+  const message = $('.contact-us__message');
+  const form = $('.contact-us__form');
+
+  const textValidation = function() {
+    if ($(this).val() !== '') {
+      $(this).removeClass('contact-us__invalid');
+      $(this).addClass('contact-us__valid');
+    } else {
+      $(this).addClass('contact-us__invalid');
+      $(this).removeClass('contact-us__valid');
+    }
+  };
+
+  mail.blur(function() {
+    if (mail.val().search(regexp) === 0) {
+      mail.removeClass('contact-us__invalid');
+      mail.addClass('contact-us__valid');
+
+      $('.contact-us__input-container')
+        .removeClass('contact-us__input-container--error');
+    } else {
+      mail.addClass('contact-us__invalid');
+      mail.removeClass('contact-us__valid');
+
+      $('.contact-us__input-container')
+        .addClass('contact-us__input-container--error');
+    }
+  });
+  name.blur(textValidation);
+  message.blur(textValidation);
+
+  mail.focus(function() {
+    $('.contact-us__input-container')
+      .removeClass('contact-us__input-container--error');
+  });
+
+  form.submit(function(event) {
+    event.preventDefault();
+
+    if (mail.val().search(regexp) !== 0 || mail.val() === '') {
+      mail.addClass('contact-us__invalid');
+      mail.removeClass('contact-us__valid');
+
+      $('.contact-us__input-container')
+        .addClass('contact-us__input-container--error');
+    }
+
+    if (name.val() === '') {
+      name.addClass('contact-us__invalid');
+      name.removeClass('contact-us__valid');
+    }
+
+    if (message.val() === '') {
+      message.addClass('contact-us__invalid');
+      message.removeClass('contact-us__valid');
+    }
+
+    if (name.val() !== ''
+      && message.val() !== ''
+      && mail.val().search(regexp) === 0
+    ) {
+      name.val('');
+      name.removeClass('contact-us__invalid');
+      name.removeClass('contact-us__valid');
+      message.val('');
+      message.removeClass('contact-us__invalid');
+      message.removeClass('contact-us__valid');
+      mail.val('');
+      mail.removeClass('contact-us__invalid');
+      mail.removeClass('contact-us__valid');
+
+      $('.contact-us__input-container')
+        .removeClass('contact-us__input-container--error');
+    }
+  });
 });
